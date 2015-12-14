8a077690054085d352e6f1d8493efa958ac39c66
zmjios <zmjios@mjzeng.cn1.global.ctrip.com>
2015-12-14 15:29:22 +0800

    给UIView添加类别画线

--- LineTest/ViewController.swift
+++ LineTest/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        let redView:UIView = UIView.init(frame: CGRectMake(100, 100, 200, 200))
+        self.view.addSubview(redView);
+        
+        
+        redView.drawLineInPosition(CTLinePosition.Top, color:UIColor.redColor(), width: (1.0 / UIScreen.mainScreen().scale));
+        redView.drawLineInPosition(CTLinePosition.Left, color: UIColor.redColor(), width: 0.5);
+        
+        
+        
+        let blueView:UIView = UIView.init(frame: CGRectMake(100, 310, 200, 5));
+        blueView.layer.borderWidth = 0.5;
+        blueView.layer.borderColor = UIColor.redColor().CGColor
+        self.view.addSubview(blueView)
     }
 
     override func didReceiveMemoryWarning() {
